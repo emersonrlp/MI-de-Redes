19ffc6bddb2aa95d0903d740af57b468635e7947
emersonrlp <102042566+emersonrlp@users.noreply.github.com>
2024-04-08 21:57:01 -0300

    criando o esqueleto do dispositivo

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -3,15 +3,21 @@ import socket
 # Configurações do servidor
 HOST = '192.168.1.106'  # Endereço IP do servidor
 PORT = 65432        # Porta a ser usada
-MESSAGE = b'Hello, TCP Server!'
 
 # Criação do socket TCP
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # Conecta ao servidor
-    s.connect((HOST, PORT))
-    # Envia mensagem para o servidor
-    s.sendall(MESSAGE)
-    # Recebe resposta do servidor
-    data = s.recv(1024)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conecta ao servidor
+s.connect((HOST, PORT))
+MESSAGE = input('Digite: ligar/Desligar/solicitar_temperatura')
+MESSAGE = bytes(MESSAGE, 'utf-8')
+# Envia mensagem para o servidor
+s.sendall(MESSAGE)
+
+# Recebe resposta do servidor
+data = s.recv(1024)
 
 print('Mensagem recebida do servidor:', data.decode())
+
+# Fecha o socket
+s.close()
--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -1,24 +1,60 @@
 import socket
+import random
 
-# Configurações do servidor
-HOST = '0.0.0.0'  # Endereço IP do servidor
-PORT = 65432        # Porta a ser usada
+def generate_temperature():
+    return round(random.uniform(20, 30), 2)  # Gera um número aleatório entre 20 e 30 com duas casas decimais
 
-# Criação do socket TCP
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+def main():
+    # Configurações do servidor
+    HOST = '0.0.0.0'  # Endereço IP do servidor
+    PORT = 65432        # Porta a ser usada
+
+    # Flag para definir se pode mandar dados de temperatura
+    can_send = 1
+
+    # Criação do socket TCP
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     # Associa o socket ao endereço e porta
     s.bind((HOST, PORT))
+
     # Habilita o servidor para aceitar conexões
     s.listen()
     print('Aguardando conexões...')
-    # Aceita a conexão do cliente
-    conn, addr = s.accept()
-    with conn:
+
+    while True:
+        # Aceita a conexão do cliente
+        conn, addr = s.accept()
         print('Conectado por', addr)
-        while True:
-            # Recebe dados do cliente
-            data = conn.recv(1024)
-            if not data:
-                break
-            # Envia dados de volta para o cliente
-            conn.sendall(data)
+
+        # Recebe dados do cliente
+        data = conn.recv(1024)
+        if not data:
+            break
+        # Verifica se a mensagem do cliente é para solicitar temperatura
+        if data.decode() == "solicitar_temperatura" and can_send == 1:
+            # Exemplo de uso:
+            data = str(generate_temperature())
+            # Decodifica para uma sequência de bytes
+            data = bytes(data + '°', 'utf-8')
+        elif data.decode() == "desligar" and can_send == 1:
+            # Se o cliente enviar o comando para desligar, não envia nenhum dado de temperatura
+            can_send = 0
+            data = b'Servidor desligando...'
+        elif data.decode() == "ligar" and can_send == 0:
+            # Se o cliente enviar o comando para ligar, o sensor pode enviar dado de temperatura
+            can_send = 1
+            data = b'Servidor ligando...'
+        else:
+            # Caso contrário, envia uma mensagem informando que o comando é inválido
+            data = b'Comando invalido...'
+            
+        # Envia dados de volta para o cliente
+        conn.sendall(data)
+
+    # Fecha a conexão com o cliente
+    conn.close()
+
+# Chamada da função principal
+if __name__ == "__main__":
+    main()
