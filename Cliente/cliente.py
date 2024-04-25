3ec7293a36e8771bca254966d0206268ae2072e6
emersonrlp <102042566+emersonrlp@users.noreply.github.com>
2024-04-25 19:02:01 -0300

    concertando dockerfile do dispositivo

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -2,15 +2,16 @@ from flask import Flask, jsonify, request
 import os
 import requests
 
-url = "http://192.168.1.106:8081/sensores"
-url_solicitacoes = "http://192.168.1.106:8081/solicitacoes"
+ip = '172.16.103.12'
+url = f"http://{ip}:8081/sensores"
+url_solicitacoes = f"http://{ip}:8081/solicitacoes"
 
 def menu():
     global url_solicitacoes
     while True:
         limpar_terminal()
         while True:
-            entrada = input("############################################################################\n#       Para listar os sensores conectados digite               (1);       #\n#       Para solicitar a temperatura de um dos sensores digite  (2);       #\n#       Para solicitar o desligamento de  um sensor digite      (3);       #\n#       Para solicitar o ligamento de um sensor digite          (4).       #\n==>")
+            entrada = input("############################################################################\n#       Para listar os sensores conectados digite               (1);       #\n#       Para solicitar a temperatura de um dos sensores digite  (2);       #\n#       Para solicitar o desligamento de  um sensor digite      (3);       #\n#       Para solicitar o ligamento de um sensor digite          (4).       #\n\n==>")
             try:
                 num1 = int(entrada)
                 if 2 <= num1 <= 4:
@@ -20,10 +21,12 @@ def menu():
                     # Exemplo de uso
                     sensores = obter_lista_sensores()
                     if sensores is not None:
-                        print("\nLista de Sensores")
+                        limpar_terminal()
+                        print("----------------------------------------------------------------------------\n                            Lista de Sensores\n----------------------------------------------------------------------------")
                         for i in sensores:
-                            print("",i)
-                        input("Precione enter para continuar!")
+                            print(f"       {i}")
+                        print("----------------------------------------------------------------------------\n")
+                        input("Precione enter para voltar ao menu!")
                         limpar_terminal()
                 else:
                     limpar_terminal()
@@ -34,17 +37,21 @@ def menu():
         
         while True:
             try:
-                entrada = input("Digite o numero do sensor: ")
+                entrada = input("\nDigite o numero do sensor: ")
                 num2 = int(entrada)  # Tenta converter a entrada para um inteiro
+                limpar_terminal()
                 break  # Sai do loop se a conversão for bem-sucedida
+                
             except ValueError:
                 print("Entrada inválida. Por favor, digite um número inteiro.")
+                limpar_terminal()
+            
         if num1 == 2:
             sensores = obter_lista_sensores()
             for i in sensores:
                 if i["id"] == num2:
-                    print(i["temperatura"])
-                    input("Precione enter para continuar!")
+                    print(f"\nTemperatura do {num2}° sensor = {i['temperatura']}")
+                    input("\nPrecione enter para voltar ao menu!")
         elif num1 == 3:
             # Dados do novo sensor a serem enviados
             novo_sensor = {"id": "", "num": num2, "Comando": 'desligar'}  # Suponha que você está adicionando um novo sensor com ID 4
@@ -60,7 +67,7 @@ def menu():
         limpar_terminal()
 
 def obter_lista_sensores():
-    url = "http://192.168.1.106:8081/sensores"
+    global url
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
